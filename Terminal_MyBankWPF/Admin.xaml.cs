ae4b803ef709aa9b8af632dca46cc9cde765ac3e
SaMuRaYX2 <lukyanenko.vlad87@gmail.com>
2024-09-27 18:54:36 +0300

    Програма почата, написаний лише шаблон вікон та встановлюється по-трози зв'язок з базою даних MySql

---
+++ Terminal_MyBankWPF/Admin.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Terminal_MyBankWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public MainWindow window_start { get; private set; }
+        public Admin(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            window_start = mainWindow;
+            this.Closed += Admin_Closed;
+            foreach(var textBox in GetAllTextBoxes(this))
+            {
+                textBox.GotFocus += TextBox_GotFocus;
+                textBox.LostFocus += TextBox_LostFocus;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                if(textBox.Tag.ToString() == "pid" || textBox.Tag.ToString() == "number_telephone" || textBox.Tag.ToString() == "pin" || textBox.Tag.ToString() == "telephone" || textBox.Tag.ToString() == "age" || textBox.Tag.ToString() == "sex")
+                {
+                    textBox.Text = textBox.Tag.ToString();
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            if(textBox != null)
+            {
+                if(textBox.Tag.ToString() == "pid" || textBox.Tag.ToString() == "number_telephone" || textBox.Tag.ToString() == "pin" || textBox.Tag.ToString() == "telephone" || textBox.Tag.ToString() == "age" || textBox.Tag.ToString() == "sex")
+                {
+                    textBox.Text = "";
+                }
+            }
+        }
+
+        private void Admin_Closed(object? sender, EventArgs e)
+        {
+            window_start.Show();
+            this.Close();
+        }
+        public IEnumerable<TextBox> GetAllTextBoxes (DependencyObject parent)
+        {
+            if(parent == null)
+            {
+                yield break;
+            }
+            List<TextBox> textBoxes = new List<TextBox>();
+            for(int i=0;i< VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if(child is TextBox textBox)
+                {
+                    yield return textBox;
+                }
+                foreach(var childofChild in GetAllTextBoxes(child))
+                {
+                    yield return childofChild;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Terminal_MyBankWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAnimatedGif;
+
+namespace Terminal_MyBankWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button_exit.MouseDown += Button_exit_MouseDown;
+            background_image.Loaded += Start_Program;
+            admin_panel.MouseDown += Admin_panel_MouseDown;
+        }
+
+        private void Admin_panel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Admin Panel_admin = new Admin(this);
+            Panel_admin.Show();
+            this.Hide();
+        }
+
+        private void Button_exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void Start_Program(object sender, RoutedEventArgs e)
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("C:\\Users\\Sam\\Documents\\Labs\\DotNetLab1\\Image\\Background_gif.gif");
+            image.EndInit();
+            ImageBehavior.SetAnimatedSource(background_image, image);
+        }
+    }
+}
